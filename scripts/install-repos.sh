5241fd5ec7bac6775ced49743982cc3127ee4c73
salverius-tech <salverius-tech@users.noreply.github.com>
2023-09-16 14:17:38 -0700

    Updated install-repos script

--- scripts/install-repos.sh
+++ scripts/install-repos.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 if [ ! -d "/app/.repos/nxs-homelab-ansible" ] ; then
   git clone ssh://git@172.30.40.51:2222/salverius/nxs-homelab-ansible.git /app/.repos/nxs-homelab-ansible
 else
